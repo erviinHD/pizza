6ba69bd5c72966629af135cd288366e8f397774d
Ervin Cabascango <ervin.cabascango.s@gmail.com>
2020-01-24 07:41:40 -0500

    Actualizacion de datos

--- resolvers.js
+++ resolvers.js
@@ -21,7 +21,29 @@ const pizzaResolver = {
             WHERE pizza_ingredients.ingredient_id = ingredient.id 
                 and pizza_ingredients.pizza_id = $1;`;
             return executeQuery(query, pizza1.id);
-            
+
+        }
+    },
+
+    Mutation: {
+        async createPizza(root, { pizza }) {
+            if (pizza == undefined) return null;
+            const query = `INSERT INTO pizza (name,origin) values ($1, $2) returning *;`
+
+            let res = await db.one(query, [pizza.name, pizza.origin]);
+          
+
+            //// insertar ingredientes
+
+            if(res.id &&  pizza.ingredients && pizza.ingredients.length>0){
+                pizza.ingredients.forEach(ingredientId => {
+                    const query = `INSERT INTO pizza_ingredients (pizza_id, ingredient_id )
+                    VALUES ($1, $2)`;
+                    executeQuery(query, [res.id, ingredientId]);
+                    
+                });
+            }
+            return res;
         }
     }
 
